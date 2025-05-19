acda94fa98fcdcdfdc465aac63cacbe2be442397
mari-mainarte <mariana.mainarte@etec.sp.gov.br>
2025-05-19 08:58:32 -0300

    :sparkles:Feat: Atualizar aula 12

---
+++ AppRpgEtec/ViewModels/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AppRpgEtec.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ AppRpgEtec/ViewModels/Usuarios/UsuarioViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AppRpgEtec.Models;
+using AppRpgEtec.Services.Usuarios;
+
+namespace AppRpgEtec.ViewModels.Usuarios
+{
+    public class UsuarioViewModel : BaseViewModel
+    {
+        private UsuarioService uService;
+
+        public ICommand AutenticarCommand {  get; set; }
+
+        #region AtributosPropriedades
+
+        private string login = string.Empty;
+        public string Login { 
+            get { return login; } 
+            set 
+            { 
+                login = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string senha = string.Empty;
+
+        public string Senha { 
+            get { return senha; }
+            set
+            {
+                senha = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        public async Task AutenticarUsuario()
+        {
+            try
+            {
+                Usuario u = new Usuario();
+                u.Username = Login;
+                u.PasswordString = Senha;
+
+                Usuario uAutenticado = await uService.PostAutenticarUsuarioAsync(u);
+
+                if (!string.IsNullOrEmpty(uAutenticado.Token))
+                {
+                    string mensagem = $"Bem-vindo(a) {uAutenticado.Username}.";
+
+                    Preferences.Set("UsuarioId", uAutenticado.Id);
+                    Preferences.Set("UsuarioUsername", uAutenticado.Username);
+                    Preferences.Set("UsuarioPerfil", uAutenticado.Perfil);
+                    Preferences.Set("UsuarioToken", uAutenticado.Token);
+
+                    await Application.Current.MainPage
+                        .DisplayAlert("Informação", mensagem, "Ok");
+
+                    Application.Current.MainPage = new MainPage();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Informação", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+        public UsuarioViewModel()
+        {
+            uService = new UsuarioService();
+            InicializarCommands();
+        }
+
+        public void InicializarCommands()
+        {
+            AutenticarCommand = new Command(async () => await AutenticarUsuario());
+        }
+
+        #region Métodos
+        public async Task RegistrarUsuario()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Informação", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+        #endregion
+    }
+}
