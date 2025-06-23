f4a0a5cc7f6a86ee50985dd7b9cc6040c1cdc5e7
mari-mainarte <mariana.mainarte@etec.sp.gov.br>
2025-06-23 03:04:21 -0300

    :sparkles: Feat: Correção no login usuario

--- AppRpgEtec/AppRpgEtec/Models/Personagem.cs
+++ AppRpgEtec/AppRpgEtec/Models/Personagem.cs
@@ -11,7 +11,7 @@ namespace AppRpgEtec.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public string PontosVida { get; set; }
+        public int PontosVida { get; set; }
         public int Forca { get; set; }
         public int Defesa { get; set; }
         public int Inteligencia { get; set; }
